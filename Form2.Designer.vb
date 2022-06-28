7c0e0204f109a1c0345ba479987c466cafc26a4d
Abiona Olushola Jude <abionaolushola11@yahoo.com>
2022-06-28 04:50:48 +0100

    add vs calculator

---
+++ Form2.Designer.vb
@@ -0,0 +1,358 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtl1 = New System.Windows.Forms.TextBox()
+        Me.txtl2 = New System.Windows.Forms.TextBox()
+        Me.txtl3 = New System.Windows.Forms.TextBox()
+        Me.txtl4 = New System.Windows.Forms.TextBox()
+        Me.txtbg1 = New System.Windows.Forms.TextBox()
+        Me.txtbg2 = New System.Windows.Forms.TextBox()
+        Me.txtbg3 = New System.Windows.Forms.TextBox()
+        Me.txtbg4 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.txtArea = New System.Windows.Forms.TextBox()
+        Me.txtLA = New System.Windows.Forms.TextBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.txtcntE = New System.Windows.Forms.TextBox()
+        Me.txtcntN = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(25, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(98, 21)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Length A-B"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(25, 77)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(105, 21)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Length B-C "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(25, 114)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(102, 21)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Length C-D"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(25, 155)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(100, 21)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Length D-A"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(364, 41)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(106, 21)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Bearing A-B"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(364, 77)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(108, 21)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Bearing B-C"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(364, 114)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(110, 21)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Bearing C-D"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(364, 155)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(108, 21)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "Bearing D-A"
+        '
+        'txtl1
+        '
+        Me.txtl1.Location = New System.Drawing.Point(147, 33)
+        Me.txtl1.Name = "txtl1"
+        Me.txtl1.Size = New System.Drawing.Size(139, 29)
+        Me.txtl1.TabIndex = 8
+        '
+        'txtl2
+        '
+        Me.txtl2.Location = New System.Drawing.Point(147, 74)
+        Me.txtl2.Name = "txtl2"
+        Me.txtl2.Size = New System.Drawing.Size(139, 29)
+        Me.txtl2.TabIndex = 9
+        '
+        'txtl3
+        '
+        Me.txtl3.Location = New System.Drawing.Point(147, 111)
+        Me.txtl3.Name = "txtl3"
+        Me.txtl3.Size = New System.Drawing.Size(139, 29)
+        Me.txtl3.TabIndex = 10
+        '
+        'txtl4
+        '
+        Me.txtl4.Location = New System.Drawing.Point(147, 155)
+        Me.txtl4.Name = "txtl4"
+        Me.txtl4.Size = New System.Drawing.Size(139, 29)
+        Me.txtl4.TabIndex = 11
+        '
+        'txtbg1
+        '
+        Me.txtbg1.Location = New System.Drawing.Point(498, 33)
+        Me.txtbg1.Name = "txtbg1"
+        Me.txtbg1.Size = New System.Drawing.Size(137, 29)
+        Me.txtbg1.TabIndex = 12
+        '
+        'txtbg2
+        '
+        Me.txtbg2.Location = New System.Drawing.Point(498, 74)
+        Me.txtbg2.Name = "txtbg2"
+        Me.txtbg2.Size = New System.Drawing.Size(137, 29)
+        Me.txtbg2.TabIndex = 13
+        '
+        'txtbg3
+        '
+        Me.txtbg3.Location = New System.Drawing.Point(498, 114)
+        Me.txtbg3.Name = "txtbg3"
+        Me.txtbg3.Size = New System.Drawing.Size(137, 29)
+        Me.txtbg3.TabIndex = 14
+        '
+        'txtbg4
+        '
+        Me.txtbg4.Location = New System.Drawing.Point(498, 155)
+        Me.txtbg4.Name = "txtbg4"
+        Me.txtbg4.Size = New System.Drawing.Size(137, 29)
+        Me.txtbg4.TabIndex = 15
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(60, 237)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(56, 21)
+        Me.Label9.TabIndex = 16
+        Me.Label9.Text = "AREA"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(540, 449)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(95, 50)
+        Me.Button2.TabIndex = 20
+        Me.Button2.Text = "="
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(191, 449)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(95, 50)
+        Me.Button3.TabIndex = 22
+        Me.Button3.Text = "&BACK"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(28, 449)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(95, 50)
+        Me.Button4.TabIndex = 23
+        Me.Button4.Text = "&CLEAR"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(31, 342)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(136, 21)
+        Me.Label10.TabIndex = 24
+        Me.Label10.Text = "Linear Accuracy"
+        '
+        'txtArea
+        '
+        Me.txtArea.Location = New System.Drawing.Point(24, 261)
+        Me.txtArea.Multiline = True
+        Me.txtArea.Name = "txtArea"
+        Me.txtArea.Size = New System.Drawing.Size(143, 27)
+        Me.txtArea.TabIndex = 26
+        '
+        'txtLA
+        '
+        Me.txtLA.Location = New System.Drawing.Point(28, 366)
+        Me.txtLA.Multiline = True
+        Me.txtLA.Name = "txtLA"
+        Me.txtLA.Size = New System.Drawing.Size(143, 27)
+        Me.txtLA.TabIndex = 27
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(312, 237)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(84, 21)
+        Me.Label12.TabIndex = 31
+        Me.Label12.Text = "Northing "
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(514, 237)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(74, 21)
+        Me.Label13.TabIndex = 32
+        Me.Label13.Text = "Easting "
+        '
+        'txtcntE
+        '
+        Me.txtcntE.Location = New System.Drawing.Point(518, 261)
+        Me.txtcntE.Multiline = True
+        Me.txtcntE.Name = "txtcntE"
+        Me.txtcntE.Size = New System.Drawing.Size(139, 132)
+        Me.txtcntE.TabIndex = 33
+        '
+        'txtcntN
+        '
+        Me.txtcntN.Location = New System.Drawing.Point(312, 261)
+        Me.txtcntN.Multiline = True
+        Me.txtcntN.Name = "txtcntN"
+        Me.txtcntN.Size = New System.Drawing.Size(139, 132)
+        Me.txtcntN.TabIndex = 34
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(353, 449)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(95, 50)
+        Me.Button1.TabIndex = 35
+        Me.Button1.Text = "&EXIT"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 21.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Sienna
+        Me.ClientSize = New System.Drawing.Size(684, 511)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtcntN)
+        Me.Controls.Add(Me.txtcntE)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.txtLA)
+        Me.Controls.Add(Me.txtArea)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.txtbg4)
+        Me.Controls.Add(Me.txtbg3)
+        Me.Controls.Add(Me.txtbg2)
+        Me.Controls.Add(Me.txtbg1)
+        Me.Controls.Add(Me.txtl4)
+        Me.Controls.Add(Me.txtl3)
+        Me.Controls.Add(Me.txtl2)
+        Me.Controls.Add(Me.txtl1)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(5)
+        Me.Name = "Form2"
+        Me.Text = "Traverse Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtl1 As TextBox
+    Friend WithEvents txtl2 As TextBox
+    Friend WithEvents txtl3 As TextBox
+    Friend WithEvents txtl4 As TextBox
+    Friend WithEvents txtbg1 As TextBox
+    Friend WithEvents txtbg2 As TextBox
+    Friend WithEvents txtbg3 As TextBox
+    Friend WithEvents txtbg4 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label10 As Label
+    Friend WithEvents txtArea As TextBox
+    Friend WithEvents txtLA As TextBox
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents txtcntE As TextBox
+    Friend WithEvents txtcntN As TextBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Form2.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form2
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim lat1, lat2, lat3, lat4 As Double
+        Dim dep1, dep2, dep3, dep4 As Double
+        Dim EL As Double
+        Dim EDP As Double
+        lat1 = (Val(txtl1.Text) * (Math.Cos(Val(txtbg1.Text))))
+        lat2 = (Val(txtl2.Text) * (Math.Cos(Val(txtbg2.Text))))
+        lat3 = (Val(txtl3.Text) * (Math.Cos(Val(txtbg3.Text))))
+        lat4 = (Val(txtl4.Text) * (Math.Cos(Val(txtbg4.Text))))
+        dep1 = (Val(txtl1.Text) * (Math.Sin(Val(txtbg1.Text))))
+        dep2 = (Val(txtl2.Text) * (Math.Sin(Val(txtbg2.Text))))
+        dep3 = (Val(txtl3.Text) * (Math.Sin(Val(txtbg3.Text))))
+        dep4 = (Val(txtl4.Text) * (Math.Sin(Val(txtbg4.Text))))
+        'THIS IS FOR THE AREA
+        txtArea.Text = (Val(txtl1.Text) * Val(txtl2.Text) * Val(txtl3.Text) * Val(txtl4.Text))
+        MsgBox("The Length = " & txtArea.Text)
+        'THIS IS FOR THE LINEAR ACCURACY
+        txtLA.Text = Math.Sqrt(Math.Pow(EL, 2) + Math.Pow(EDP, 2))
+        MsgBox("Linear Misclosure =" & txtLA.Text)
+        'GETTING THE NORTHING AND EASTING
+        EL = lat1 + lat2 + lat3 + lat4
+        EDP = dep1 + dep2 + dep3 + dep4
+        If EL = 0 Then
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        txtArea.Text = ""
+        txtbg1.Text = ""
+        txtbg2.Text = ""
+        txtbg3.Text = ""
+        txtbg4.Text = ""
+        txtcntE.Text = ""
+        txtcntN.Text = ""
+        txtl1.Text = ""
+        txtl2.Text = ""
+        txtl3.Text = ""
+        txtl4.Text = ""
+        txtLA.Text = ""
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Button1.Text = MsgBox("Do you want to exit?", vbYesNo)
+        If Button1.Text = vbYes Then
+            Application.Exit()
+        End If
+    End Sub
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
